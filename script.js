3d8b13e0ac25f023c2058b1a773089e4be939727
SophGits <sophie.jiffy@gmail.com>
2015-01-13 23:01:17 +0000

    Sketch out what digital cv might look like.

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  var videos = {
+    kusiri: 'http://www.youtube.com/embed/qiaZ8S7amS4',
+    outstanding: 'http://www.youtube.com/embed/zJ-4muRmqBM'
+  }
+
+  $('li div').on('click', function(){
+    $('li div').removeClass('selected');
+    $(this).addClass('selected');
+    var id = $(this).attr('id');
+    var videoSrc = videos[id];
+    $('iframe').attr('src', videoSrc);
+  });
+
+});

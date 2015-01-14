3df80f469fe35f3bce2786794e0b06504358e71a
SophGits <sophie.jiffy@gmail.com>
2015-01-14 18:10:21 +0000

    Load all videos from start.

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  var videos = {
-    kusiri: 'http://www.youtube.com/embed/qiaZ8S7amS4',
-    outstanding: 'http://www.youtube.com/embed/zJ-4muRmqBM'
-  }
+  // var videos = {
+  //   kusiri: 'http://www.youtube.com/embed/qiaZ8S7amS4',
+  //   outstanding: 'http://www.youtube.com/embed/zJ-4muRmqBM',
+  //   comicreader: 'http://www.youtube.com/embed/jBeZjb68eks'
+  // }
 
-  $('li div').on('click', function(){
-    $('li div').removeClass('selected');
-    $(this).addClass('selected');
-    var id = $(this).attr('id');
-    var videoSrc = videos[id];
-    if(videoSrc){
-      $('iframe').removeClass('hide').attr('src', videoSrc);
-      $('.right p').removeClass('hide').addClass('hide');
-    } else {
-      $('iframe').removeClass('hide').addClass('hide');
-      $('.right p').removeClass('hide');
-    }
-  });
+  // $('li div').on('click', function(){
+  //   $('li div').removeClass('selected');
+  //   $(this).addClass('selected');
+  //   var id = $(this).attr('id');
+  //   var videoSrc = videos[id];
+  //   if(videoSrc){
+  //     $('iframe').removeClass('hide').attr('src', videoSrc);
+  //     $('.right p').removeClass('hide').addClass('hide');
+  //   } else {
+  //     $('iframe').removeClass('hide').addClass('hide');
+  //     $('.right p').removeClass('hide');
+  //   }
+  // });
+
+
+$('li div').on('click', function(){
+  $('li div').removeClass('selected');
+  $(this).addClass('selected');
+  var name = $(this).attr('id');
+
+  var namelength = name.length;
+
+  var iframes = $('iframe');
+  $.each(iframes, function(i, v){
+    $(this).removeClass('hide').addClass('hide');
+  })
+
+ $('iframe').filter(function (){
+     var classes = $(this).attr('class').split(' ');
+     for (var i=0; i<classes.length; i++){
+       if (classes[i].slice(0, namelength) === name){
+          $(this).removeClass('hide');
+          return true;
+       }
+     }
+     console.log('false');
+     return false;
+ });
+});
+
 
 });
